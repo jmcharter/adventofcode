36601ff3dbceb2c8ff46660757c4be2a96f0c698
Jon <jon@fedora.fritz.box>
2023-07-20 09:24:24 +0100

    Add day 1 solution

--- 2019/d01/src/main.rs
+++ 2019/d01/src/main.rs
@@ -3,6 +3,7 @@ fn main() {
     let p1 = p1(&input);
     let p2 = p2(&input);
     println!("P1: {p1}");
+    println!("P2: {p2}");
 }
 
 fn p1(input: &str) -> i32 {
@@ -13,6 +14,25 @@ fn p1(input: &str) -> i32 {
     })
 }
 
-fn p2(input: &str) {
+fn p2(input: &str) -> u32 {
     let lines = input.lines();
+    lines.fold(0, |acc, e| {
+        let val = e.parse::<u32>().unwrap();
+        let mut sub_req = get_fuel_req(val);
+        let mut req = sub_req;
+        while sub_req > 0 {
+            sub_req = get_fuel_req(sub_req);
+            req += sub_req;
+        }
+        acc + req
+    })
+}
+
+fn get_fuel_req(mass: u32) -> u32 {
+    let div_result = mass / 3;
+    if div_result < 2 {
+        0
+    } else {
+        div_result - 2
+    }
 }
