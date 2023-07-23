344eee9af82f13806f557586ae5f3a263e0429a2
Jon <jon.charter@habitat.energy>
2023-07-23 22:55:16 +0100

    Add day 02 part 02 solution

--- 2019/d02/src/main.rs
+++ 2019/d02/src/main.rs
@@ -9,7 +9,7 @@ enum OpCode {
 }
 
 impl OpCode {
-    fn from_i32(value: i32) -> Option<Self> {
+    fn from_u32(value: u32) -> Option<Self> {
         match value {
             1 => Some(Self::Add),
             2 => Some(Self::Multiply),
@@ -25,17 +25,17 @@ fn read_input() -> std::io::Result<String> {
     Ok(input)
 }
 
-fn part_one(input: String) {
-    let mut integers: Vec<i32> = input
+fn part_one(input: &String, noun: u32, verb: u32) -> u32 {
+    let mut integers: Vec<u32> = input
         .trim()
         .split(',')
         .map(|e| e.parse().unwrap())
         .collect();
-    integers[1] = 12;
-    integers[2] = 2;
+    integers[1] = noun;
+    integers[2] = verb;
     let mut position = 0;
     while position < integers.len() {
-        let op_code = OpCode::from_i32(integers[position]).unwrap();
+        let op_code = OpCode::from_u32(integers[position]).unwrap();
         if op_code == OpCode::Halt {
             break;
         }
@@ -44,10 +44,10 @@ fn part_one(input: String) {
         let in1_i: usize = integers[position + 1] as usize;
         let in2_i: usize = integers[position + 2] as usize;
         let out_i: usize = integers[position + 3] as usize;
-        let in1: i32 = integers[in1_i];
-        let in2: i32 = integers[in2_i];
+        let in1: u32 = integers[in1_i];
+        let in2: u32 = integers[in2_i];
 
-        let result: i32 = match op_code {
+        let result: u32 = match op_code {
             OpCode::Add => in1 + in2,
             OpCode::Multiply => in1 * in2,
             _ => break,
@@ -56,12 +56,23 @@ fn part_one(input: String) {
         integers[out_i] = result;
         position += 4
     }
-    println!("{}", integers[0]);
+    integers[0]
 }
 
-// fn part_two() {}
+fn part_two(input: &String) -> u32 {
+    for noun in 0..99 {
+        for verb in 0..99 {
+            let result = part_one(&input, noun, verb);
+            if result == 19690720 {
+                return 100 * noun + verb;
+            }
+        }
+    }
+    0
+}
 
 fn main() {
     let input = read_input().unwrap();
-    part_one(input);
+    println!("Part one: {:?}", part_one(&input, 12, 2));
+    println!("Part two: {:?}", part_two(&input));
 }
