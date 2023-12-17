5fef23bfeab9c065a9f36f354d405d069fda8c90
Jon <jon@chrt.uk>
2023-12-17 12:20:32 +0000

    d16 init

---
+++ 2023/d16/src/main.rs
@@ -0,0 +1,46 @@
+use std::{
+    env, fs,
+    io::{self, BufRead, BufReader, Read},
+};
+
+fn main() -> io::Result<()> {
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file1 = fs::File::open(filename)?;
+    // let file2 = fs::File::open(filename)?;
+    let reader1 = BufReader::new(file1);
+    // let reader2 = BufReader::new(file2);
+
+    println!("Part one: {}", process_part_one(reader1));
+    // println!("Part two: {}", process_part_two(reader2));
+    Ok(())
+}
+
+fn parse_data<R: Read>(reader: BufReader<R>) -> Vec<String> {}
+
+fn process_part_one<R: Read>(reader: BufReader<R>) -> u64 {
+    0
+}
+
+// fn process_part_two<R: Read>(reader: BufReader<R>) -> u64 {
+//     0
+// }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const INPUT: &str = "";
+
+    #[test]
+    fn test_process_part_one() {
+        let input_bytes = INPUT.as_bytes();
+        assert_eq!(0, process_part_one(BufReader::new(input_bytes)));
+    }
+
+    // #[test]
+    // fn test_process_part_two() {
+    //     let input_bytes = INPUT.as_bytes();
+    //     assert_eq!(71503, process_part_two(BufReader::new(input_bytes)));
+    // }
+}
